f7bd4fe88e7d2874347044f6ddc8bd0b089de840
Félix Piédallu <felix@piedallu.me>
2024-01-13 12:24:29 +0100

    Packaging v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+#REMOVEME? YNH_PHP_VERSION="7.3"
 
 # Composer version
 YNH_COMPOSER_VERSION="2.1.3"
-
-pkg_dependencies="mutt"
 
 #=================================================
 # PERSONAL HELPERS
